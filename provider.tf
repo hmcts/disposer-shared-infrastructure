ea9f85eded401cbe20ec7257a20decc3f01fef14
Priyanka Verma <priyanka.verma@hmcts.net>
2023-06-12 16:40:57 +0100

    Revert "lau-630 disposer-shared-infrastructure"
    This reverts commit 19bd50043bc800d38e65637467f23c935ce47b74.

--- provider.tf
+++ provider.tf
@@ -1,22 +1,15 @@
-provider "azurerm" {
-  features {}
+terraform {
+
+  required_version = ">=1.2.0"
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.19.1"
+    }
+  }
 }
 
 provider "azurerm" {
-  features {}
-  alias           = "aks"
-  subscription_id = var.aks_subscription_id
-  skip_provider_registration = false
-}
-
-provider "azurerm" {
-  alias           = "aks-preview"
-  subscription_id = var.aks_preview_subscription_id
-  features {}
-}
-
-provider "azurerm" {
-  alias           = "mgmt"
-  subscription_id = var.mgmt_subscription_id
   features {}
 }
