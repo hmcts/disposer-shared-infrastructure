75077589ddb509ef43813b238bf74701a94cad6e
58060 <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
2024-08-06 13:38:36 +0000

    Updating Terraform Formatting

--- alerts.tf
+++ alerts.tf
@@ -3,9 +3,9 @@ locals {
 }
 
 module "idam-user-disposer-action-group-slack-email" {
-  source   = "git@github.com:hmcts/cnp-module-action-group"
-  location = "global"
-  env      = "prod"
+  source                 = "git@github.com:hmcts/cnp-module-action-group"
+  location               = "global"
+  env                    = "prod"
   resourcegroup_name     = azurerm_resource_group.rg.name
   action_group_name      = "Idam User Disposer Slack Email Alert - ${var.env}"
   short_name             = "dispr-alert"
@@ -14,15 +14,15 @@ module "idam-user-disposer-action-group-slack-email" {
 }
 
 module "idam-user-disposer-run-failure-alert" {
-  source                     = "git@github.com:hmcts/cnp-module-metric-alert"
-  location                   = var.location
-  app_insights_name          = "disposer-${var.env}"
-  alert_name                 = "${var.application_name}-${var.env}-run-failure"
-  alert_desc                 = "Alert when idam user disposer fail to run"
-  app_insights_query         = "traces | where message contains 'Disposer Idam User Summary :'"
-  custom_email_subject       = "Alert: Idam user disposer run failure in disposer-${var.env}"
+  source               = "git@github.com:hmcts/cnp-module-metric-alert"
+  location             = var.location
+  app_insights_name    = "disposer-${var.env}"
+  alert_name           = "${var.application_name}-${var.env}-run-failure"
+  alert_desc           = "Alert when idam user disposer fail to run"
+  app_insights_query   = "traces | where message contains 'Disposer Idam User Summary :'"
+  custom_email_subject = "Alert: Idam user disposer run failure in disposer-${var.env}"
   #run every 6 hrs for early alert
-  frequency_in_minutes       = "360"
+  frequency_in_minutes = "360"
   # window of 1 day as data extract needs to run daily
   time_window_in_minutes     = "1440"
   severity_level             = "2"
