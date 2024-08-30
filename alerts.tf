ef614e242f6fbc6ac88e06fce0447f9997b5cc4d
Priyanka Verma <priyanka.verma@hmcts.net>
2024-08-30 17:02:12 +0100

    Creating alerts early

--- alerts.tf
+++ alerts.tf
@@ -17,7 +17,7 @@ module "idam-user-disposer-service-failure-alert" {
   alert_desc           = "Alert when idam user disposer fail to run"
   app_insights_query   = "traces | where message contains 'Error executing Disposer Idam User service' | where toint(dayofweek(timestamp)/1d) < 5"
   custom_email_subject = "Alert: Idam user disposer failure in disposer-${var.env}"
-  #run every 6 hours for early alert
+  #run every 18 hours for early alert
   frequency_in_minutes = var.disposer_frequency_in_minutes
   # window of 1 day as data extract needs to run daily
   time_window_in_minutes     = var.disposer_time_window_in_minutes
